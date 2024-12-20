312a2f05053a334f2733ef042aed505701024cec
Landvoigt <tim.voigt@ik.me>
2024-12-20 20:59:57 +0100

    added activate action to admin panel selection

--- category/admin.py
+++ category/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from .models import Category
 
 
+def mark_active(modeladmin, request, queryset):
+    updated_count = queryset.update(active=True)
+    modeladmin.message_user(request, f"{updated_count} successfully activated categories.")
+
+mark_active.short_description = "Activate selected categories"
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'active', 'title', 'description', 'creation_date')
+    actions = [mark_active]
 
 admin.site.register(Category, CategoryAdmin)
--- command/admin.py
+++ command/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from .models import Command
 
 
+def mark_active(modeladmin, request, queryset):
+    updated_count = queryset.update(active=True)
+    modeladmin.message_user(request, f"{updated_count} successfully activated commands.")
+
+mark_active.short_description = "Activate selected commands"
+
 class CommandAdmin(admin.ModelAdmin):
     list_display = ('id', 'active', 'command', 'category', 'example', 'creation_date')
+    actions = [mark_active]
 
 admin.site.register(Command, CommandAdmin)
