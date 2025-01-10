c88704c1966a44cc07cecb16ab3580b09c61f005
Landvoigt <tim.voigt@ik.me>
2025-01-10 22:56:29 +0100

    fixed staticfiles

--- knowledge_base/settings.py
+++ knowledge_base/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -158,7 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/srv/projects/knowledge_base/knowledge_base_backend/knowledge_base/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
