98ea8496ca42d6e4a4088e79db380b97a333ee20
Landvoigt <tim.voigt@ik.me>
2025-01-28 22:29:41 +0100

    removed unused views

--- devknowhow/settings.py
+++ devknowhow/settings.py
@@ -169,6 +169,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# DIRECT_ACTIVATION_PASSWORD = env("DIRECT_ACTIVATION_PASSWORD")
-GITHUB_SECRET = env("GITHUB_SECRET")
--- devknowhow/urls.py
+++ devknowhow/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webhook/', views.github_webhook, name='github_webhook'),
     path('', include('category.urls')),
     path('', include('command.urls')),
     path('', include('routine.urls')),
