cd69ccaa1c6d5976be9e0d302053b27b33b22646
Landvoigt <tim.voigt@ik.me>
2025-12-29 16:15:10 +0100

    added psql

--- command/admin.py
+++ command/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
+
+from category.models import Category
 from .models import Command
 
 
@@ -13,6 +15,30 @@ def deactivate(modeladmin, request, queryset):
 
 activate.short_description = "Activate selected commands"
 deactivate.short_description = "Deactivate selected commands"
+
+def make_add_to_category_action(category):
+    def action(modeladmin, request, queryset):
+        for command in queryset:
+            command.category.add(category)
+        modeladmin.message_user(
+            request,
+            f"{queryset.count()} commands added to category '{category.title}'."
+        )
+    action.short_description = f"Add commands to category '{category.title}'"
+    action.__name__ = f"add_to_category_{category.id}"
+    return action
+
+def make_remove_from_category_action(category):
+    def action(modeladmin, request, queryset):
+        for command in queryset:
+            command.category.remove(category)
+        modeladmin.message_user(
+            request,
+            f"{queryset.count()} commands removed from category '{category.title}'."
+        )
+    action.short_description = f"Remove commands from category '{category.title}'"
+    action.__name__ = f"remove_from_category_{category.id}"
+    return action
 
 class CommandAdmin(TranslationAdmin):
     list_display = ('id', 'active', 'title', 'category_list', 'copy_count', 'created_at',)
@@ -67,5 +93,9 @@ class CommandAdmin(TranslationAdmin):
             if alternatives.count() == 1:
                 kwargs["initial"] = alternatives
         return super().alternative_field(db_field, request, **kwargs)
+
+for cat in Category.objects.all():
+    CommandAdmin.actions.append(make_add_to_category_action(cat))
+    CommandAdmin.actions.append(make_remove_from_category_action(cat))
 
 admin.site.register(Command, CommandAdmin)
--- devknowhow/settings.py
+++ devknowhow/settings.py
@@ -120,12 +120,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'devknowhow.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
+            'PORT': os.environ.get('POSTGRES_PORT'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB_PROD'),
+            'USER': os.environ.get('POSTGRES_USER_PROD'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD_PROD'),
+            'HOST': os.environ.get('POSTGRES_HOST_PROD'),
+            'PORT': os.environ.get('POSTGRES_PORT_PROD'),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
